2d970a16257cbf8f902823336d89a78b01ca02b4
Ingeniero2 <Ingeniero2@MATRITEC.LOCAL>
2024-08-23 16:56:37 -0400

    Checkout mas responsivo

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,17 +1,24 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartContext } from "../context/CartContext"
 import { Box, Button, Flex, Table, TableContainer, Tbody, Text, Th, Thead, Tr } from "@chakra-ui/react"
 import { CartDrawerTable } from "./CartDrawerTable"
 
 export const Checkout = () => {
-
     const { cart, totalPrice } = useContext(CartContext)
 
+    const [width, setWidth] = useState(window.innerWidth)
+
+	const handleResize = () => {
+		setWidth(window.innerWidth)
+	}
+	window.addEventListener('resize', handleResize)
+
+    
     return (
-        <Box display='flex' flexDirection='column' mt='4.5em'>
+        <Box display='flex'  mt='4.5em' flexDirection='column'>
             <Text fontSize='2xl' alignSelf='center'> Carrito</Text>
-            <Flex m='10' justifyContent='space-between'>
-                <TableContainer m='1' w='60%'>
+            <Flex m='10' flexDirection={ width < 800 ? 'column' : 'start'}>
+                <TableContainer m='1' w={ width < 800 ? '100%' : '70%'}>
                     <Table size='sm' variant='simple'>
                         <Thead>
                             <Tr>
@@ -33,11 +40,11 @@ export const Checkout = () => {
                         </Tbody>
                     </Table>
                 </TableContainer>
-                <Flex w='30%' flexDirection='column' alignContent='center'>
+                <Flex  w={ width < 800 ? '100%' : '30%'} flexDirection='column' alignContent='center' m='2' justify='center'>
                     <Flex justifyContent='space-around' m='5'>
-                        <Text fontSize='2xl'>Total</Text > <Text fontSize='2xl'>US ${totalPrice.toFixed(2)}</Text>
+                        <Text fontSize='2xl'>Total</Text > 
+                        <Text fontSize='2xl'>US ${totalPrice.toFixed(2)}</Text>
                     </Flex>
-                    <Text fontSize='2xl'>Costo de envío calculado en el siguiente paso.</Text>
                     <Button colorScheme="green" mt='4'>FINALIZAR COMPRA</Button>
                 </Flex>
             </Flex>
